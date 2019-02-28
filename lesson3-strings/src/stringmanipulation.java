82ebdcf414123126b67eabb900b6706c2f58ba79
msingh <msingh@HS-D210-20.MOHS.local>
2019-02-28 14:20:01 -0500

    gg

--- lesson3-strings/src/stringmanipulation.java
+++ lesson3-strings/src/stringmanipulation.java
@@ -23,6 +23,13 @@ System.out.println("a\\\\");
 System.out.println("\\\n\\");
 
 System.out.println("I \"love\" \n");
-		
+
+String s = "The number of rabbits is";
+int argh = 129;
+System.out.println(s + " "+ argh + ".");
+String g = "Computer Science is for nerds";
+System.out.println(g.substring(0,29).toLowerCase());
+String m = "Look here!";
+System.out.println(m.length());
 	}
 }
