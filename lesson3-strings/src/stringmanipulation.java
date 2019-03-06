b61a3268e820baa0cf89851d850ba49bc9605b63
msingh <msingh@HS-D210-20.MOHS.local>
2019-03-06 14:19:56 -0500

    g

--- lesson3-strings/src/stringmanipulation.java
+++ lesson3-strings/src/stringmanipulation.java
@@ -29,7 +29,25 @@ int argh = 129;
 System.out.println(s + " "+ argh + ".");
 String g = "Computer Science is for nerds";
 System.out.println(g.substring(0,29).toLowerCase());
-String m = "Look here!";
+String m = "\"Look here!\"";
 System.out.println(m.length());
+System.out.println(m.substring(0,12) + "has" + " " +  m.length() + " " + "characters" + ".");
+System.out.println("All \"good\" men should to the aid of their country.");
+System.out.println("Hello \nHello again");
+System.out.println("A backslash looks like this \\, ...right?");
+String pq = "Eddie Haskel";
+int hm = pq.length();
+String ed = pq.substring(hm-4);
+System.out.println(ed);
+
+String s1 = "Allan Alda";
+String s2 = "John Wayne";
+String s3 = "Gregory Peck";
+int i1 = s1.length();
+String s1a = s1.substring(i1-8);
+System.out.println(s1a);
+int i2 = s2.length();
+String s2a = s2.substring(i2-8);
+System.out.println(s2a);
 	}
 }
